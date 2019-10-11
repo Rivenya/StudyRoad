4792a21a731d8409ce819ff3538769a03d9279ea
rivenya <alifetimetogether@outlook.com>
2019-10-11 22:41:04 +0800

    图的遍历

--- 数据结构与算法/graph/graph.js
+++ 数据结构与算法/graph/graph.js
@@ -1,19 +1,19 @@
-function Graph () {
-  let map = [];
-  let node_num = 0;
-  let edge_num = 0;
-  this.init = function (props) {
-    map = props;
-    node_num = this.get_node_num();
-    edge_num = this.get_edge_num();
+function Graph() {
+  let map = []
+  let node_num = 0
+  let edge_num = 0
+  this.init = function(props) {
+    map = props
+    node_num = this.get_node_num()
+    edge_num = this.get_edge_num()
   }
-  this.get_node_num = function () {
+  this.get_node_num = function() {
     if (node_num != 0) {
       return node_num
     }
     return map.length
   }
-  this.get_edge_num = function () {
+  this.get_edge_num = function() {
     if (edge_num != 0) {
       return edge_num
     }
@@ -27,7 +27,7 @@ function Graph () {
     }
     return num
   }
-  function graph_dfs (num, array, path) {
+  function graph_dfs(num, array, path) {
     array[num] = 1
     path.push(num)
     for (let i = 0; i < map[num].length; i++) {
@@ -36,8 +36,7 @@ function Graph () {
       }
     }
   }
-  function graph_bfs (num, path, array, list) {
-    console.log(num)
+  function graph_bfs(num, path, array, list) {
     if (array[num] === 0) {
       path.push(num)
       list.push(num)
@@ -55,7 +54,7 @@ function Graph () {
       graph_bfs(list[0], path, array, list)
     }
   }
-  this.dfs = function (i) {
+  this.dfs = function(i) {
     let path = []
     let array = new Array(map.length)
     for (let i = 0; i < array.length; i++) {
@@ -64,7 +63,7 @@ function Graph () {
     graph_dfs(i, array, path)
     return path
   }
-  this.bfs = function (i) {
+  this.bfs = function(i) {
     let path = []
     let list = []
     let array = new Array(map.length)
@@ -81,11 +80,12 @@ var test_data = [
   [28, 0, 16, '#', '#', '#', 14],
   ['#', 16, 0, 12, '#', '#', '#'],
   ['#', '#', 12, 0, '#', 22, 18],
-  ['#', '#', '#', 22, 0, 25, '#'],
-  [10, '#', '#', '#', 25, 0, 24],
-  ['#', 14, '#', 18, '#', 24, 0],
+  [10, '#', '#', '#', 0, 25, '#'],
+  ['#', '#', '#', 22, 25, 0, 24],
+  ['#', 14, '#', 18, '#', 24, 0]
 ]
 
 var z = new Graph()
 z.init(test_data)
-console.log(z.bfs(0))
+console.log(z.bfs(4))
+console.log(z.dfs(2))
