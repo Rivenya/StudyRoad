26206aadd65a71d0e86f16d1d861b571c627aaad
rivenya <alifetimetogether@outlook.com>
2019-11-03 14:07:14 +0800

    12

---
+++ Vue/demo/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <p>这是主页</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  beforeRouteEnter (to, from, next) {
+    console.log("this is a beforeRouteEnter")
+    next()
+  },
+  beforeRouteLeave (to, from, next) {
+    alert("确定要离开？")
+    next()
+  }
+}
+</script>
